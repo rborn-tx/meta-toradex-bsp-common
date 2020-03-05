rborn-tx/meta-toradex-bsp-common

commit f22318fe5a27258a02a9923629c9b8399b387d92

toradex-kernel-localversion.bbclass: use SRCREV to set scmversion
In commit 32df2d41:
[ toradex-kernel-localversion.bbclass: use origin branch set scmversion ]

we changed to use origin branch to set scmversion, but that's not good
enough, we should stick to SRCREV which is the latest original commit
in the source.

Related-to: TOR-694
Signed-off-by: Ming Liu <ming.liu@toradex.com>


(cherry picked from commit a49daf2f134e019d528566cf1903d4a3ce211297)

classes/toradex-kernel-localversion.bbclass
@@ -21,10 +21,10 @@ kernel_do_configure_append() {
 	sed -i -e /CONFIG_LOCALVERSION_AUTO/d ${B}/.config
 	if [ "${SCMVERSION}" = "y" ]; then
 		# Add GIT revision to the local version
-		if [ -n "${KBRANCH}" ]; then
-			head=`git --git-dir=${S}/.git rev-parse --verify --short origin/${KBRANCH} 2> /dev/null`
-		elif [ -n "${SRCBRANCH}" ]; then
-			head=`git --git-dir=${S}/.git rev-parse --verify --short origin/${SRCBRANCH} 2> /dev/null`
+		if [ -n "${SRCREV_machine}" -a "${SRCREV_machine}" != "INVALID" ]; then
+			head=`git --git-dir=${S}/.git rev-parse --verify --short ${SRCREV_machine} 2> /dev/null`
+		elif [ -n "${SRCREV}" -a "${SRCREV}" != "INVALID" ]; then
+			head=`git --git-dir=${S}/.git rev-parse --verify --short ${SRCREV} 2> /dev/null`
 		else
 			head=`git --git-dir=${S}/.git rev-parse --verify --short HEAD 2> /dev/null`
 		fi

commit 3e645e66daea9951a02b9de0f52864a79389e23a

toradex-kernel-localversion.bbclass: fix set scmversion with autorev
Handle the case where SRCREV is set to AUTOREV.
fixes: f22318f toradex-kernel-localversion.bbclass: use SRCREV to set scmversion

Related-to: TOR-694
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

classes/toradex-kernel-localversion.bbclass
@@ -23,6 +23,9 @@ kernel_do_configure_append() {
 		# Add GIT revision to the local version
 		if [ -n "${SRCREV_machine}" -a "${SRCREV_machine}" != "INVALID" ]; then
 			head=`git --git-dir=${S}/.git rev-parse --verify --short ${SRCREV_machine} 2> /dev/null`
+		elif [ -n "${SRCREV}" -a "${SRCREV}" = "AUTOINC" ]; then
+			branch=`git --git-dir=${S}/.git  symbolic-ref --short -q HEAD`
+			head=`git --git-dir=${S}/.git rev-parse --verify --short origin/${branch} 2> /dev/null`
 		elif [ -n "${SRCREV}" -a "${SRCREV}" != "INVALID" ]; then
 			head=`git --git-dir=${S}/.git rev-parse --verify --short ${SRCREV} 2> /dev/null`
 		else