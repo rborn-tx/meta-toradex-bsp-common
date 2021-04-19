rborn-tx/meta-toradex-bsp-common

commit 9e9859a9d3cb5a6daa2a8a7e0c98d5830021b644

image_type_tezi.bbclass: introduce TEZI_AUTO_INSTALL
This makes it possible to build a image tarball with 'autoinstall'
enabled.

Related-to: TEI-773

Signed-off-by: Ming Liu <ming.liu@toradex.com>

classes/image_type_tezi.bbclass
@@ -27,6 +27,7 @@ TEZI_ROOT_LABEL ??= "RFS"
 TEZI_ROOT_NAME ??= "rootfs"
 TEZI_ROOT_SUFFIX ??= "tar.xz"
 TEZI_USE_BOOTFILES ??= "true"
+TEZI_AUTO_INSTALL ??= "false"
 TEZI_BOOT_SUFFIX ??= "${@'bootfs.tar.xz' if oe.types.boolean('${TEZI_USE_BOOTFILES}') else ''}"
 TEZI_CONFIG_FORMAT ??= "2"
 # Require newer Tezi for mx8 Socs with the u-boot environment bugfix
@@ -274,7 +275,7 @@ def rootfs_tezi_json(d, flash_type, flash_data, json_file, uenv_file):
     from datetime import datetime
 
     deploydir = d.getVar('DEPLOY_DIR_IMAGE')
-    data = OrderedDict({ "config_format": d.getVar('TEZI_CONFIG_FORMAT'), "autoinstall": False })
+    data = OrderedDict({ "config_format": d.getVar('TEZI_CONFIG_FORMAT'), "autoinstall": oe.types.boolean(d.getVar('TEZI_USE_BOOTFILES')) })
 
     # Use image recipes SUMMARY/DESCRIPTION...
     data["name"] = d.getVar('SUMMARY')

commit b960cee9fba7f3f9a6603cd5464fcfa45df3bceb

image_type_tezi.bbclass: fix a typo
A typo was introduced by commit 9e9859a9:
[ image_type_tezi.bbclass: introduce TEZI_AUTO_INSTALL ]

TEZI_USE_BOOTFILES should be TEZI_AUTO_INSTALL.

Related-to: TEI-773

Signed-off-by: Ming Liu <ming.liu@toradex.com>

classes/image_type_tezi.bbclass
@@ -275,7 +275,7 @@ def rootfs_tezi_json(d, flash_type, flash_data, json_file, uenv_file):
     from datetime import datetime
 
     deploydir = d.getVar('DEPLOY_DIR_IMAGE')
-    data = OrderedDict({ "config_format": d.getVar('TEZI_CONFIG_FORMAT'), "autoinstall": oe.types.boolean(d.getVar('TEZI_USE_BOOTFILES')) })
+    data = OrderedDict({ "config_format": d.getVar('TEZI_CONFIG_FORMAT'), "autoinstall": oe.types.boolean(d.getVar('TEZI_AUTO_INSTALL')) })
 
     # Use image recipes SUMMARY/DESCRIPTION...
     data["name"] = d.getVar('SUMMARY')