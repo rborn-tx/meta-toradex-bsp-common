rborn-tx/meta-toradex-bsp-common

commit 23ade8864c7d81adf4d8caf6f77f93bf7ae64167

image_type_tezi: : follow change to vendor subdir for dtbs
With moving to kernel 6.5 the kernel commit 724ba67515320 ("ARM: dts:
Move .dts files to vendor sub-directories") introduced a regression
with our 32 bit machines.

Use 'make_dtb_boot_files' to get the basenames of the dtb list also
for NAND based modules.

Related-to: ELB-5263
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

classes/image_type_tezi.bbclass
@@ -5,9 +5,10 @@
 # Since it also generates the image.json description file it is rather
 # interwind with the boot flow which is U-Boot target specific.
 
+KERNEL_DEVICETREE_BASENAME = "${@make_dtb_boot_files(d)}"
 WKS_FILE_DEPENDS:append = " tezi-metadata virtual/dtb"
 DEPENDS += "${WKS_FILE_DEPENDS}"
-IMAGE_BOOT_FILES_REMOVE = "${@make_dtb_boot_files(d) if d.getVar('KERNEL_IMAGETYPE') == 'fitImage' else ''}"
+IMAGE_BOOT_FILES_REMOVE = "${KERNEL_DEVICETREE_BASENAME} if d.getVar('KERNEL_IMAGETYPE') == 'fitImage' else ''}"
 IMAGE_BOOT_FILES:append = " overlays.txt ${@'' if d.getVar('KERNEL_IMAGETYPE') == 'fitImage' else 'overlays/*;overlays/'}"
 IMAGE_BOOT_FILES:remove = "${IMAGE_BOOT_FILES_REMOVE}"
 
@@ -364,7 +365,7 @@ python rootfs_tezi_run_json() {
         flash_data = rootfs_tezi_rawnand(d)
         uenv_file = d.getVar('UBOOT_ENV_TEZI_RAWNAND')
         uboot_file = d.getVar('UBOOT_BINARY_TEZI_RAWNAND')
-        artifacts += " " + d.getVar('KERNEL_IMAGETYPE') + " " + d.getVar('KERNEL_DEVICETREE')
+        artifacts += " " + d.getVar('KERNEL_IMAGETYPE') + " " + d.getVar('KERNEL_DEVICETREE_BASENAME')
     elif flash_type == "emmc":
         use_bootfiles = oe.types.boolean(d.getVar('TEZI_USE_BOOTFILES'))
         flash_data = rootfs_tezi_emmc(d, use_bootfiles)

commit e4b2438b5c1147ad7775609dc6ba31ac28b1a64c

image_type_tezi: fix dtb deployment
The rework for 32bit dtb vendor subdirs wrongly stops deploying the dtbs.
This due to not following the correct bitbake syntax.

Fixes: 23ade88 ("image_type_tezi: : follow change to vendor subdir for dtbs")
Related-to: ELB-5263
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

classes/image_type_tezi.bbclass
@@ -8,7 +8,7 @@
 KERNEL_DEVICETREE_BASENAME = "${@make_dtb_boot_files(d)}"
 WKS_FILE_DEPENDS:append = " tezi-metadata virtual/dtb"
 DEPENDS += "${WKS_FILE_DEPENDS}"
-IMAGE_BOOT_FILES_REMOVE = "${KERNEL_DEVICETREE_BASENAME} if d.getVar('KERNEL_IMAGETYPE') == 'fitImage' else ''}"
+IMAGE_BOOT_FILES_REMOVE = "${@'${KERNEL_DEVICETREE_BASENAME}' if d.getVar('KERNEL_IMAGETYPE') == 'fitImage' else ''}"
 IMAGE_BOOT_FILES:append = " overlays.txt ${@'' if d.getVar('KERNEL_IMAGETYPE') == 'fitImage' else 'overlays/*;overlays/'}"
 IMAGE_BOOT_FILES:remove = "${IMAGE_BOOT_FILES_REMOVE}"
 