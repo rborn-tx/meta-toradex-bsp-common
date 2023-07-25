rborn-tx/meta-toradex-bsp-common

commit 8154788a0cfcb950e104da7b67cc7244e8e3cdf6

linux-firmware: bump imx-firmware to latest version
This bumps imx-firmware to latest lf-6.1.22_2.0.0.

Related-to: ELB-5247
Signed-off-by: Andrejs Cainikovs <andrejs.cainikovs@toradex.com>

recipes-kernel/linux-firmware/linux-firmware_%.bbappend
@@ -1,5 +1,5 @@
 IMX_FIRMWARE_SRC ?= "git://github.com/NXP/imx-firmware.git;protocol=https"
-SRCBRANCH_imx-firmware = "lf-5.15.52_2.1.0"
+SRCBRANCH_imx-firmware = "lf-6.1.22_2.0.0"
 SRC_URI += " \
     ${IMX_FIRMWARE_SRC};branch=${SRCBRANCH_imx-firmware};destsuffix=imx-firmware;name=imx-firmware \
 "

commit 337a077d0cf63aea1c104bf9e8c730810721206a

linux-firmware: bump imx-firmware srcrev
Recent change switched the branch of imx-firmware to the
latest release (lf-6.1.22_2.0.0), but did not update srcrev,
effectively making no change to the target image at all.
This bumps imx-firmware srcrev to the latest commit on the
mentioned branch.

Fixes: 8154788a0cfc ("linux-firmware: bump imx-firmware to latest version")

Related-to: ELB-5247
Signed-off-by: Andrejs Cainikovs <andrejs.cainikovs@toradex.com>

recipes-kernel/linux-firmware/linux-firmware_%.bbappend
@@ -4,7 +4,7 @@ SRC_URI += " \
     ${IMX_FIRMWARE_SRC};branch=${SRCBRANCH_imx-firmware};destsuffix=imx-firmware;name=imx-firmware \
 "
 
-SRCREV_imx-firmware = "b6f070e3d4cab23932d9e6bc29e3d884a7fd68f4"
+SRCREV_imx-firmware = "f775d53ca3a478c85e8c8a880e44cc269bd14db0"
 
 SRCREV_FORMAT = "default_imx-firmware"
 