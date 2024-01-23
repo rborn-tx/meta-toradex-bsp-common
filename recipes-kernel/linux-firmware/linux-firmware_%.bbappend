rborn-tx/meta-toradex-bsp-common

commit a7100f0ca20b6f6b2b61bbe112028cd3c703598b

linux-firmware: update imx-firmware to lf-6.1.55-2.2.0
Update imx-firmware to commit 7be5a936ce8677962dd7b41c6c9f41dd14350bec
on branch lf-6.1.55_2.2.0. This updates the IW416 Bluetooth and Wi-Fi
Firmware to 16.92.21.p84.128.

Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>

recipes-kernel/linux-firmware/linux-firmware_%.bbappend
@@ -1,10 +1,10 @@
 IMX_FIRMWARE_SRC ?= "git://github.com/NXP/imx-firmware.git;protocol=https"
-SRCBRANCH_imx-firmware = "lf-6.1.22_2.0.0"
+SRCBRANCH_imx-firmware = "lf-6.1.55_2.2.0"
 SRC_URI += " \
     ${IMX_FIRMWARE_SRC};branch=${SRCBRANCH_imx-firmware};destsuffix=${S}/imx-firmware;name=imx-firmware \
 "
 
-SRCREV_imx-firmware = "f775d53ca3a478c85e8c8a880e44cc269bd14db0"
+SRCREV_imx-firmware = "7be5a936ce8677962dd7b41c6c9f41dd14350bec"
 
 SRCREV_FORMAT = "default_imx-firmware"
 

commit 239237c27511902a9d7b430e44386791b8fa8fbc

linux-firmware: fix imx-firmware license rename/update
This fixes NXP having renamed and updated the license (again).

Fixes: a7100f0ca20b ("linux-firmware: update imx-firmware to lf-6.1.55-2.2.0")
Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>

recipes-kernel/linux-firmware/linux-firmware_%.bbappend
@@ -9,9 +9,9 @@ SRCREV_imx-firmware = "7be5a936ce8677962dd7b41c6c9f41dd14350bec"
 SRCREV_FORMAT = "default_imx-firmware"
 
 do_install:append () {
-    # Install IMX Firmware EULA license
+    # Install IMX Firmware EULA/LICENSE license
     install -d ${D}${nonarch_base_libdir}/firmware
-    install -m 0644 imx-firmware/EULA.txt ${D}${nonarch_base_libdir}/firmware/LICENSE.imx
+    install -m 0644 imx-firmware/LICENSE.txt ${D}${nonarch_base_libdir}/firmware/LICENSE.imx
 
     # Install common IMX firmware files
     install -d ${D}${nonarch_base_libdir}/firmware/nxp
@@ -35,9 +35,9 @@ LICENSE += " \
     & firmware-imx \
 "
 LIC_FILES_CHKSUM += " \
-    file://imx-firmware/EULA.txt;md5=673fa34349fa40f59e0713cb0ac22b1f \
+    file://imx-firmware/LICENSE.txt;md5=2827219e81f28aba7c6a569f7c437fa7 \
 "
-NO_GENERIC_LICENSE[firmware-imx] = "imx-firmware/EULA.txt"
+NO_GENERIC_LICENSE[firmware-imx] = "imx-firmware/LICENSE.txt"
 
 PACKAGES =+ " \
     ${PN}-imx-common \