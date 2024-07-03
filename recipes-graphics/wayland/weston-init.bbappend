rborn-tx/meta-toradex-bsp-common

commit 42440669ef89c952ea1b184b2a624186085d941d

weston-init: add a 3s sleep before starting weston
We see on Verdin AM62 and Verdin iMX8MM that often weston fails as
the drm device is not yet ready. Work around this by giving the
drm device 3 more seconds to be up and running.

Related-to: ELB-5782
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-graphics/wayland/weston-init.bbappend
@@ -22,5 +22,10 @@ do_install:append:tdx() {
         sed -i "s/Group=weston/Group=root/" ${D}${systemd_system_unitdir}/weston.service
         sed -i "s/SocketUser=weston/SocketUser=root/" ${D}${systemd_system_unitdir}/weston.socket
         sed -i "s/SocketGroup=wayland/SocketGroup=root/" ${D}${systemd_system_unitdir}/weston.socket
+
+        # We see on Verdin AM62 and Verdin iMX8MM that often weston fails as
+        # the drm device is not yet ready. Work around this by giving the
+        # drm device 3 more seconds to be up and running.
+	sed -i "/^EnvironmentFile=.etc.default.weston/a ExecStartPre=-\/usr\/bin\/sleep 3" ${D}${systemd_system_unitdir}/weston.service
     fi
 }

commit ace1ba50fd92a1acb452955a9a24af15dbaeca48

weston-init: increas sleep before starting weston to 5 sec
We see on Verdin AM62 and Verdin iMX8MM that often weston fails as
the drm device is not yet ready. Work around this by giving the
drm device 5 more seconds to be up and running.
The 3 seconds were not enough for the AM62 with single core.

Fixes: 42440669ef89 ("weston-init: add a 3s sleep before starting weston")
Related-to: ELB-5782
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-graphics/wayland/weston-init.bbappend
@@ -25,7 +25,7 @@ do_install:append:tdx() {
 
         # We see on Verdin AM62 and Verdin iMX8MM that often weston fails as
         # the drm device is not yet ready. Work around this by giving the
-        # drm device 3 more seconds to be up and running.
-	sed -i "/^EnvironmentFile=.etc.default.weston/a ExecStartPre=-\/usr\/bin\/sleep 3" ${D}${systemd_system_unitdir}/weston.service
+        # drm device 5 more seconds to be up and running.
+        sed -i "/^EnvironmentFile=.etc.default.weston/a ExecStartPre=-\/usr\/bin\/sleep 5" ${D}${systemd_system_unitdir}/weston.service
     fi
 }